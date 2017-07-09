8d4125aae5667939c07bee498ad1bf0ca946d762
Matt Woods <mattwoods21@gmail.com>
2017-07-08 22:58:50 -0500

    init commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var todoList = [
+	'Walk the dog',
+	'Pick up some milk',
+	'Put the kids to bed',
+	'Pour yourself a beer'
+];
+
+var todo = document.getElementById('todo');
+
+function drawTodoList(){
+	// first delete old list
+	todo.innerHTML = ' ';
+	// Now add updated array to new list
+	for(var i = 0; i < todoList.length; i++){
+	todo.innerHTML += "<li>" + todoList[i] + "</li>";
+	}
+}
+
+function addTodoItem(){
+	var action = document.getElementById("activity");
+
+	if(action.value === ''){
+		alert("Enter a task first.");
+	} else{
+		todoList.push(action.value);
+		action.value = '';
+		drawTodoList();
+	}
+}
